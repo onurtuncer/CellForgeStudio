0d4618d3436b8ca8639d169266fb1e024d18ae6d
Onur Tuncer <onur.tuncer@milteksan.com>
2026-06-16 13:15:17 +0300

    feat: add WpfBridge C++/CLI CLR class library scaffold

---
+++ WpfBridge/Bridge.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "Bridge.h"
+
+using namespace System;
+using namespace WpfBridge;
+
+String^ Bridge::GetMessage()
+{
+	return "Hello from WpfBridge";
+}
---
+++ WpfBridge/Bridge.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using namespace System;
+
+namespace WpfBridge {
+
+	public ref class Bridge
+	{
+	public:
+		static String^ GetMessage();
+	};
+}
