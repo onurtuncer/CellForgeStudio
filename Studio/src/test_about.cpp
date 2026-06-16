90db9c3711b8a7dadc45017f6b32a19629dbd529
Onur Tuncer <onur.tuncer@milteksan.com>
2026-06-16 14:02:43 +0300

    test: add native about_test executable that calls ShowWpfAboutDialog; hook into CMake build

---
+++ Studio/src/test_about.cpp
@@ -0,0 +1,32 @@
+#include <windows.h>
+
+typedef void (__cdecl *ShowWpfAboutDialogFn)(HWND);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	const wchar_t CLASS_NAME[] = L"AboutTestWindowClass";
+
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = DefWindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"About Test Host", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, nullptr, nullptr, hInstance, nullptr);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	HMODULE h = LoadLibraryW(L"WpfBridge.dll");
+	if (h)
+	{
+		auto fn = (ShowWpfAboutDialogFn)GetProcAddress(h, "ShowWpfAboutDialog");
+		if (fn) fn(hwnd);
+		FreeLibrary(h);
+	}
+
+	// Exit immediately after dialog closes
+	return 0;
+}
